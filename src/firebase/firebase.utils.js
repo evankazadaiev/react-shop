8e56b3f3b4174b16092b32435214db6b8c83052c
Ivan <ivan.kazadaev@technorely.com>
2019-12-26 20:02:18 +0200

    added sign up component, handle sign up with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,32 @@ const config = {
   appId: "1:264910355204:web:bb210123be85afcf0a2e51",
   measurementId: "G-FE12CXFL4K"
 };
-// Initialize Firebase
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+  
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+  const snapShot = await userRef.get();
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return userRef;
+  
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
